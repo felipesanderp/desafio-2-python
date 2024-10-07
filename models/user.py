539641207820c545874c91dbf1c04ead4d6c91ad
felipesanderp <felipe.panisson@hotmail.com>
2024-10-07 12:15:04 -0300

    user model

---
+++ models/user.py
@@ -0,0 +1,14 @@
+"""User model"""
+
+from flask_login import UserMixin
+from database import db
+
+
+class User(db.Model, UserMixin):
+    """User class"""
+
+    # id (int), username (text), password (text), role (text)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable=False, default="user")
